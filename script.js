a2866927454cb77b6ec0c13b3964ae32f00a7fec
jack mathew <mathewjack307@gmail.com>
2025-06-06 15:43:56 +0530

    hii

---
+++ script.js
@@ -0,0 +1,10 @@
+function greet(name,age){
+    return 'hello '  +name +' is ' +age +' old'
+}
+console.log(greet('messi',30))
+
+
+function showname(){
+    const name=document.getElementById('name').value
+    do
+} 
